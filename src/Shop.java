e3a4ca9a30d4af03f6f68ea30220796fc361c984
Dinesh Adabala <dinesh.adabala417@gmail.com>
2024-04-05 17:49:01 +0530

    hai

--- src/Shop.java
+++ src/Shop.java
@@ -4,12 +4,13 @@ public class Shop {
     double amount;
     public List<Product>productList;
 
-    public void selectionProcess(Product product){
+    public Product selectionProcess(Product product){
         if (productList.contains(product)){
             amount= product.productCost;
             System.out.println("product found" + product);
         }else {
             System.out.println("product not found");
         }
+        return product;
     }
 }
